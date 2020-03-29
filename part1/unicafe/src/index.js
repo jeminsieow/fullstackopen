01cb0a4cd8d9b6746625d7993ec68246a0599bdc
jeminsieow <sjemin7@gmail.com>
2020-03-29 16:14:14 +0800

    Completed exercises 1.6 - 1.11

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,68 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+
+const Title = ({ text }) => <h1>{text}</h1>
+
+const Button = ({ handleClick, count, text }) => (
+  <button onClick={() => handleClick(count + 1)}>{text}</button>
+)
+
+const Statistic = ({ text, value }) => {
+  if (text === "percentage") {
+    return (
+      <tr>
+        <td>{text}</td>
+        <td>{value} %</td>
+      </tr>
+    )
+  }
+  return (
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
+  )
+}
+
+const Statistics = ({ good, neutral, bad }) => {
+  const average = (good - bad) / (good + neutral + bad)
+
+  if (good + neutral + bad === 0) {
+    return (
+      <div>No feedback given</div>
+    )
+  }
+  return (
+    <div>
+      <Statistic text="good" value={good}/>
+      <Statistic text="neutral" value={neutral}/>
+      <Statistic text="bad" value={bad}/>
+      <Statistic text="all" value={good + neutral + bad}/>
+      <Statistic text="average" value={average}/>
+      <Statistic text="percentage" value={good / (good + neutral + bad) * 100}/>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>
+      <Title text="give feedback"/>
+      <Button handleClick={setGood} count={good} text="good"/>
+      <Button handleClick={setNeutral} count={neutral} text="neutral"/>
+      <Button handleClick={setBad} count={bad} text="bad"/>
+      <Title text="statistics"/>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
