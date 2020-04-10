fa1aa8db467bd243e1f3b96ee1c80a815f0ace5f
Je Min Sieow <jemin.sieow@prepr.org>
2020-04-10 16:50:28 +0800

    Completed exercise 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const PersonForm = ({ handleSubmit, handleNameChange, handleNumberChange, newName, newNumber }) => {
   return (
@@ -37,15 +38,23 @@ const Persons = ({ persons, filter }) => {
 }
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+
+  const [ persons, setPersons] = useState([])
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ filter, setFilter ] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('response', response)
+        setPersons(response.data)  
+      })
+  }, [])
+
+  console.log('render', persons.length, 'persons')
 
   const addName = (event) => {
     event.preventDefault()
